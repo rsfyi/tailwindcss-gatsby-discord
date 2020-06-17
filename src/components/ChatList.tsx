a67d01321f9d12b08c767d4f61ae5ed1649f482a
Rahul Singh <rscodedocs@gmail.com>
2020-06-17 18:55:14 +0530

    hashtag sidebar and chat list

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,98 +1,137 @@
 import React from "react"
 
+import profileImg from "../images/avatar.jpg"
+
 export const ChatList = () => {
   return (
     <div className="bg-gray-750 flex-1 flex flex-col justify-between">
-      <div className="overflow-y-auto flex-1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis animi
-        omnis a deserunt, quidem ducimus provident neque ratione ipsam
-        repudiandae sed molestiae alias placeat corporis dolorum voluptates
-        numquam ea quisquam quibusdam veritatis at ullam harum necessitatibus?
-        Reprehenderit eaque quos saepe totam labore aspernatur, perspiciatis
-        cumque cum possimus unde aut alias rem quis velit non sequi aperiam!
-        Vero, culpa ullam a consectetur facilis laudantium aut et aliquid
-        provident corporis iste reiciendis obcaecati saepe cupiditate placeat?
-        Hic doloremque perspiciatis, ad ipsam, obcaecati et corrupti nostrum
-        atque dolorem quibusdam itaque culpa animi unde commodi eligendi
-        necessitatibus iure distinctio maiores quas aut! Maxime veritatis
-        aliquam distinctio debitis dolorum totam quos cupiditate nihil
-        recusandae pariatur fuga placeat suscipit, voluptas facilis minima
-        architecto itaque unde animi. Beatae unde tempora totam dignissimos
-        eligendi impedit molestias, dolor reiciendis vero, neque minus sed
-        nostrum, laboriosam debitis ea expedita architecto officiis nisi
-        necessitatibus labore mollitia fugiat animi delectus. Quod est, sequi,
-        iusto fuga laudantium qui repellendus ratione illum doloremque dolorem,
-        corporis magnam rem ea eveniet ipsam repudiandae alias saepe quam dicta
-        officia voluptates. Sunt enim minus veniam corporis nobis aspernatur
-        animi voluptatum eius cupiditate ea veritatis, quis provident
-        praesentium est, impedit itaque fugit dicta unde, dolor ipsam
-        reprehenderit. Delectus molestiae earum a alias libero? Ratione repellat
-        delectus inventore fugit, sint, accusantium quaerat velit laborum error
-        officiis veniam odio ex doloremque aperiam, maiores neque! Earum minima
-        minus illum? Voluptates, esse natus eligendi qui, dolorum, iste
-        provident sequi assumenda corrupti illum sint alias enim ex atque?
-        Cupiditate consequatur quaerat voluptatibus commodi velit ipsam, ut
-        nihil animi non saepe amet facilis vero provident nisi officia sint
-        accusantium delectus quos nemo dolore suscipit. Rerum odio laborum
-        eveniet quasi mollitia, ea facilis tempora dolore porro nisi ducimus
-        quis, laboriosam temporibus dicta? Laborum optio, sunt earum
-        consequatur, animi, consequuntur repudiandae alias porro consectetur
-        nobis id quod officiis quas tenetur facere aspernatur qui odio. Eveniet
-        eligendi fugiat, quisquam nemo laborum molestias labore veritatis sequi
-        ut nulla doloremque reprehenderit ipsa minima, natus culpa asperiores
-        itaque architecto ex quam! Maxime, recusandae excepturi sequi voluptas
-        iste unde quibusdam id hic nulla reprehenderit accusamus, sed libero,
-        alias dicta. Aut corporis atque quam recusandae consequuntur voluptates
-        sapiente dicta provident eum praesentium culpa maxime, voluptas maiores
-        nobis sed incidunt ipsa error ipsam. Quisquam nulla laboriosam optio
-        molestiae eveniet consequatur corporis nihil aperiam? Repellat
-        praesentium, fugiat incidunt repellendus et tempora nesciunt suscipit
-        iure adipisci esse quidem eum laboriosam commodi nostrum eveniet
-        distinctio corrupti explicabo doloremque. Atque blanditiis fuga officia
-        magni rem sit facilis sunt laboriosam reprehenderit, qui beatae non ex
-        quod. Incidunt mollitia aliquam, commodi est tempore, saepe numquam
-        error nostrum rem impedit esse, pariatur quidem qui a architecto tempora
-        aut quos modi odit. Numquam quibusdam deserunt sapiente maxime, veniam
-        voluptatibus tempore repudiandae mollitia a enim animi officiis
-        voluptatum placeat dicta ipsa, fuga nisi rerum, tempora molestias!
-        Quaerat ea quasi vero laudantium eveniet quae natus quibusdam assumenda
-        optio saepe illum iusto debitis fugit cumque eligendi labore animi minus
-        officiis at recusandae incidunt consectetur, facere deleniti obcaecati.
-        Fugiat eveniet earum ad? Nobis, facilis explicabo porro deleniti vero
-        voluptas! Inventore, blanditiis? Magni, aspernatur exercitationem
-        repellendus necessitatibus deleniti assumenda libero, velit architecto
-        saepe error odio facilis ea placeat, dolorum quae. Aliquam, ipsa illum!
-        Placeat in quis impedit explicabo alias praesentium eaque distinctio
-        aliquam ut earum quo rem non voluptatibus, quidem architecto qui commodi
-        a esse quos at quibusdam quas. Quo libero, harum asperiores id saepe,
-        cumque aliquid ullam ipsa sint iste sit dolore iusto quasi illo,
-        quisquam maiores veritatis. Eveniet, error. In odio a corrupti aut amet
-        ad architecto. Praesentium rem magni minus provident nobis fugiat itaque
-        quasi, voluptates alias a ipsa quos nulla ratione reiciendis? Tenetur,
-        ut hic suscipit ullam voluptate quibusdam nobis error harum quia iusto
-        eveniet beatae? Quibusdam, asperiores mollitia vel possimus architecto
-        exercitationem distinctio repellat numquam, vero eligendi deleniti, at
-        unde ipsa. Accusamus exercitationem, perspiciatis ratione non asperiores
-        architecto ipsum fugit quibusdam deleniti corporis suscipit iste qui
-        eligendi quam distinctio. Adipisci dolorem repellat aut vero! Minima
-        explicabo veritatis adipisci repudiandae accusantium, aperiam officia
-        sit ipsa, asperiores officiis maiores libero expedita! Aliquam,
-        laboriosam porro rem unde quo dolore possimus deserunt a odit, illum qui
-        non maxime cumque eos voluptatum repellat! Et, earum dignissimos
-        praesentium error ipsam fugit voluptatibus beatae reiciendis quisquam
-        esse cumque molestias architecto labore veniam voluptates impedit soluta
-        natus? Optio, numquam dolore earum sed vero placeat at ullam, inventore
-        modi quisquam magni totam exercitationem harum autem non asperiores
-        dolores et veritatis fugiat quo officia iste voluptate? Mollitia aliquam
-        a asperiores officiis obcaecati animi odit ratione laborum dolores
-        voluptas saepe architecto facere, ex quo iure delectus quod! Ab eligendi
-        nihil reprehenderit illo? Rerum culpa in eveniet deleniti obcaecati
-        totam exercitationem explicabo? Fugit nulla ratione quasi fuga molestias
-        totam delectus, ipsum voluptatum similique. Sequi alias harum commodi
-        deleniti officia nulla voluptatem dignissimos a saepe, dolorem, iste
-        deserunt id iure laboriosam nesciunt exercitationem quos optio!
+      <div className="overflow-y-auto flex-1 text-sm text-gray-400">
+        <div className="mx-6 my-3 py-4 border-t border-gray-700 flex">
+          <div className="flex-none">
+            <a href="#">
+              <img
+                src={profileImg}
+                alt="profile"
+                className="w-10 h-10 rounded-full"
+              />
+            </a>
+          </div>
+          <div className="ml-4">
+            <div>
+              <a href="#" className="text-white hover:underline">
+                Rahul Singh
+              </a>
+              <span className="text-xs text-gray-600 ml-1">17/06/2020</span>
+            </div>
+            <div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+                fugit totam at esse numquam officia aliquam quisquam sint cumque
+                tempora
+              </div>
+              <div>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Laborum, mollitia.
+              </div>
+              <div>Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+        </div>
+        <div className="mx-6 my-3 py-4 border-t border-gray-700 flex">
+          <div className="flex-none">
+            <a href="#">
+              <img
+                src={profileImg}
+                alt="profile"
+                className="w-10 h-10 rounded-full"
+              />
+            </a>
+          </div>
+          <div className="ml-4">
+            <div>
+              <a href="#" className="text-white hover:underline">
+                Rahul Singh
+              </a>
+              <span className="text-xs text-gray-600 ml-1">17/06/2020</span>
+            </div>
+            <div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+                fugit totam at esse numquam officia aliquam quisquam sint cumque
+                tempora
+              </div>
+              <div>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Laborum, mollitia.
+              </div>
+              <div>Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+        </div>
+        <div className="mx-6 my-3 py-4 border-t border-gray-700 flex">
+          <div className="flex-none">
+            <a href="#">
+              <img
+                src={profileImg}
+                alt="profile"
+                className="w-10 h-10 rounded-full"
+              />
+            </a>
+          </div>
+          <div className="ml-4">
+            <div>
+              <a href="#" className="text-white hover:underline">
+                Rahul Singh
+              </a>
+              <span className="text-xs text-gray-600 ml-1">17/06/2020</span>
+            </div>
+            <div>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+                fugit totam at esse numquam officia aliquam quisquam sint cumque
+                tempora
+              </div>
+              <div>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Laborum, mollitia.
+              </div>
+              <div>Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="bg-gray-500">chat input</div>
+
+      <div className="bg-gray-750 flex items-center mx-3 h-24 border-t border-gray-600">
+        <button className="p-2 h-10 bg-gray-700 rounded-l text-gray-500 hover:text-white border-r border-gray-600">
+          <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+            <g data-name="Layer 2">
+              <g data-name="plus-circle">
+                <rect width="24" height="24" opacity="0"></rect>
+                <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm3 11h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2V9a1 1 0 0 1 2 0v2h2a1 1 0 0 1 0 2z"></path>
+              </g>
+            </g>
+          </svg>
+        </button>
+        <div className="flex-1">
+          <input
+            type="text"
+            className="w-full text-sm h-10 px-2 py-2 bg-gray-700 focus:outline-none text-gray-200"
+          />
+        </div>
+        <div className="px-2 py-2 bg-gray-700 rounded-r flex items-center h-10">
+          <button className="h-10 bg-gray-700 rounded-l text-gray-500 hover:text-white">
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 512 512">
+              <path d="M32 448c0 17.7 14.3 32 32 32h160V320H32v128zm448-288h-42.1c6.2-12.1 10.1-25.5 10.1-40 0-48.5-39.5-88-88-88-41.6 0-68.5 21.3-103 68.3-34.5-47-61.4-68.3-103-68.3-48.5 0-88 39.5-88 88 0 14.5 3.8 27.9 10.1 40H32c-17.7 0-32 14.3-32 32v80c0 8.8 7.2 16 16 16h480c8.8 0 16-7.2 16-16v-80c0-17.7-14.3-32-32-32zm-326.1 0c-22.1 0-40-17.9-40-40s17.9-40 40-40c19.9 0 34.6 3.3 86.1 80h-86.1zm206.1 0h-86.1c51.4-76.5 65.7-80 86.1-80 22.1 0 40 17.9 40 40s-17.9 40-40 40zm-72 320h160c17.7 0 32-14.3 32-32V320H288v160z"></path>
+            </svg>
+          </button>
+
+          <button className="h-10 bg-gray-700 rounded-l text-gray-500 hover:text-white ml-3">
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 496 512">
+              <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm80 168c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm-160 0c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32 32-32zm194.8 170.2C334.3 380.4 292.5 400 248 400s-86.3-19.6-114.8-53.8c-13.6-16.3 11-36.7 24.6-20.5 22.4 26.9 55.2 42.2 90.2 42.2s67.8-15.4 90.2-42.2c13.4-16.2 38.1 4.2 24.6 20.5z"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
