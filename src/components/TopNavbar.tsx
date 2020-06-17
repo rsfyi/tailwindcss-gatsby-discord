af9e4cab11c6477a73b77aa312b9e27847bf8cab
Rahul Singh <rscodedocs@gmail.com>
2020-06-17 12:40:27 +0530

    divided into components

---
+++ src/components/TopNavbar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const TopNavbar = () => {
+  return (
+    <div className="bg-red-500 flex">
+      <div className="bg-red-300 w-56 flex-none">tailwind css</div>
+      <div>rest of bar</div>
+    </div>
+  )
+}
